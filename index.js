b770eacc7480d6727fed79b15edeb69472538afe
Travis Springer <travis.springer@gmail.com>
2021-04-10 15:47:25 -0500

    Initial functionality for basic README creation

--- index.js
+++ index.js
@@ -1,13 +1,92 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the name of the project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Provide a brief description of the project:',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Describe the installation process (if any):',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Describe the use case for the project:',
+        name: 'usage'
+    },
+    {
+        type: 'list',
+        message: 'List any licenses that apply to your project (if any):',
+        choices: [
+            'Apache',
+            'Academic',
+            'GNU',
+            'ISC',
+            'MIT',
+            'Mozilla',
+            'Open'
+        ],
+        name: 'licenses'
+    },
+    {
+        type: 'input',
+        message: 'List all contributors:',
+        name: 'contributors'
+    },
+    {
+        type: 'input',
+        message: 'Are tests included:',
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        message: 'Enter your GitHub username:',
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: 'Enter your email address:',
+        name: 'email'
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) { return console.log(err) }
+        console.log('README file created successfully!')
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    try {
+        const responses = await inquirer.prompt(questions);
+        console.log('Here are your responses:');
+        console.log(responses);
+        console.log('Generating your README...')
+        const content = generateMarkdown(responses);
+        console.log('README generated!');
+        console.log(content);
+        await writeFileAsync('./files/README.md', content);
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,58 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
+
+const tableOfContents = `## Table of Contents
+* [Description](#discription)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributors](#contributors)
+* [Tests](#tests)
+* [Questions](#questions)`
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+## Table of Contents
+* [Description](#discription)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributors](#contributors)
+* [Tests](#tests)
+* [Questions](#questions)
 
-`;
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${data.licenses}
+
+## Contributors
+${data.contributors}
+
+## Tests
+${data.tests}
+
+## Questions
+${data.username}
+${data.email}`;
 }
 
 module.exports = generateMarkdown;
